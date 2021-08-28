e6c4a00c67cdbc7d41f609946f8b418961897b5c
elijahsamuels <elijahsamuels@gmail.com>
2021-08-27 22:21:41 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// https://api.airtable.com/v0/app00Ee4sqvmw2krS/tblAi6AIRhl0FUfhT?api_key=keyfrWpv2j3XMaL5E
+// https://api.airtable.com/v0/app00Ee4sqvmw2krS/ALL%20DATES?api_key=keyfrWpv2j3XMaL5E
+
+const airtableURL = 'https://api.airtable.com/v0/'
+const puresoulAPI = 'app00Ee4sqvmw2krS/'
+const musiciansTable = 'tblAi6AIRhl0FUfhT?'
+const datesTable = 'ALL%20DATES?'
+const puresoulAPIkey = 'api_key=keyfrWpv2j3XMaL5E'
+
+const allPureSoulPresentsMuisicians = `${airtableURL}${puresoulAPI}${musiciansTable}${puresoulAPIkey}`
+const allPureSoulPresentsDates = `${airtableURL}${puresoulAPI}${datesTable}${puresoulAPIkey}`
